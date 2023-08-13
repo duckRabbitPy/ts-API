3f1e294db5d9a8a1aa2cc0a4aed88705c4806f70
duckRabbitPy <78092825+duckRabbitPy@users.noreply.github.com>
2023-08-13 19:21:49 +0100

    update route

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -2,11 +2,8 @@ import { RequestHandler } from "express";
 import { pipe } from "@effect/data/Function";
 import * as Effect from "@effect/io/Effect";
 import * as Pg from "@sqlfx/pg";
-
-import ToDo, { parseTodo } from "../models/todos";
+import { parseTodo } from "../models/todos";
 import { Config } from "effect";
-
-const TODOs: ToDo[] = [];
 
 const PgLive = Pg.makeLayer({
   database: Config.succeed("effect_pg_dev"),
@@ -14,7 +11,7 @@ const PgLive = Pg.makeLayer({
 
 export const createToDo: RequestHandler = (req, res, next) => {
   const text = (req.body as { text: string }).text;
-  console.log("body", req.body);
+
   const insertTodo = (text: string) =>
     pipe(
       Pg.tag,
@@ -33,7 +30,7 @@ export const createToDo: RequestHandler = (req, res, next) => {
 };
 
 export const getToDo: RequestHandler = (req, res, next) => {
-  const selectAllTodos = pipe(
+  const selectAllItems = pipe(
     Pg.tag,
     Effect.flatMap(
       (sql) =>
@@ -47,32 +44,49 @@ export const getToDo: RequestHandler = (req, res, next) => {
     Effect.flatMap((todos) => Effect.succeed(res.json({ todos: todos })))
   );
 
-  pipe(selectAllTodos, Effect.provideLayer(PgLive), Effect.runPromise);
+  pipe(selectAllItems, Effect.provideLayer(PgLive), Effect.runPromise);
 };
 
 export const updateToDo: RequestHandler<{ id: string }> = (req, res, next) => {
   const todoID = req.params.id;
   const updatedText = (req.body as { text: string }).text;
-  const todoIndex = TODOs.findIndex((elem) => todoID === elem.id);
 
-  if (todoIndex < 0) {
-    throw new Error("Could not foind todo");
-  }
+  const updateItem = (id: string, text: string) =>
+    pipe(
+      Pg.tag,
+      Effect.flatMap(
+        (sql) => sql`INSERT INTO todos (id, text)
+        VALUES (${id}, ${text})
+        ON CONFLICT (id) DO UPDATE SET text = EXCLUDED.text
+        RETURNING *`
+      ),
+      Effect.map((todos) => todos.map((todo) => parseTodo(todo))),
+      Effect.flatMap((effectArr) => Effect.all(effectArr)),
+      Effect.flatMap((todos) => Effect.succeed(res.json({ todos: todos })))
+    );
 
-  TODOs[todoIndex] = new ToDo(TODOs[todoIndex].id, updatedText);
-
-  res.json({ message: "Updated todo!", updatedToDo: TODOs[todoIndex] });
+  pipe(
+    updateItem(todoID, updatedText),
+    Effect.provideLayer(PgLive),
+    Effect.runPromise
+  );
 };
 
 export const deleteToDo: RequestHandler<{ id: string }> = (req, res, next) => {
   const todoID = req.params.id;
-  const todoIndex = TODOs.findIndex((elem) => todoID === elem.id);
 
-  if (todoIndex < 0) {
-    throw new Error("Could not foind todo");
-  }
+  const deleteItem = (id: string) =>
+    pipe(
+      Pg.tag,
+      Effect.flatMap(
+        (sql) => sql`DELETE FROM todos WHERE id=${id} RETURNING *`
+      ),
+      Effect.map((todos) => todos.map((todo) => parseTodo(todo))),
+      Effect.flatMap((effectArr) => Effect.all(effectArr)),
+      Effect.flatMap((todos) =>
+        Effect.succeed(res.json({ message: "todo deleted", todos: todos }))
+      )
+    );
 
-  TODOs.slice(todoIndex, 1);
-
-  res.json({ message: "Deleted todo!", TODOs });
+  pipe(deleteItem(todoID), Effect.provideLayer(PgLive), Effect.runPromise);
 };
--- src/models/todos.ts
+++ src/models/todos.ts
@@ -1,8 +1,6 @@
 import * as Schema from "@effect/schema/Schema";
-
-export default class TodoClass {
-  constructor(public id: string, public text: string) {}
-}
+import { Row } from "@sqlfx/sql/Connection";
+import { Effect, pipe } from "effect";
 
 export const ToDoSchema = Schema.struct({
   id: Schema.number,
@@ -12,5 +10,12 @@ export const ToDoSchema = Schema.struct({
 export type Todo = Schema.To<typeof ToDoSchema>;
 
 export const parseTodo = Schema.parse(ToDoSchema);
+
+export const parseTodosFromPG = (rows: Row[]) =>
+  pipe(
+    Effect.succeed(rows),
+    Effect.map((row) => row.map((row) => parseTodo(row))),
+    Effect.flatMap((effectArr) => Effect.all(effectArr))
+  );
 
 export const todoPrinter = (todo: Todo) => `id: ${todo.id} text: ${todo.text}`;
